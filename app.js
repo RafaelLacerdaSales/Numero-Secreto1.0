8863f70b9335226a7adfdc92ca31998e211b949b
REDDEAD <hsesgamer@outlook.com>
2024-05-26 09:12:16 -0300

    Último teste

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let listaDeNumenerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let tentativas = 1;
-let quantidadeDeNumeros = 10;
+let quantidadeDeNumeros = 100;
 let numeroScreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag,texto){
